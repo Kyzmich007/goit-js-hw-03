7840b1bb887e0dc120b11436350c7b79fabe6b45
Vladyslav Kuz'menko <Kyzmich007@ukr.net>
2020-07-22 22:01:59 +0300

    task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,11 @@
 "use strict";
+const countProps = function (obj) {
+  const arrayOfValue = Object.values(obj);
+  return arrayOfValue.length;
+};
+
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: "Mango", age: 2 })); // 2
+
+console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 })); // 3
