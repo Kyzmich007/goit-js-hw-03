84d7b1567f052bbaf26d459900cea3e4c9fa938f
Vladyslav Kuz'menko <Kyzmich007@ukr.net>
2020-07-22 23:23:56 +0300

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,36 @@
 "use strict";
+const findBestEmployee = function (employees) {
+  const arrayOfValue = Object.values(employees);
+  const bigNumeric = Math.max(...arrayOfValue);
+  for (const key in employees) {
+    if (employees[key] === bigNumeric) {
+      return key;
+    }
+  }
+};
+
+console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99,
+  })
+); // lorence
+
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4,
+  })
+); // mango
+
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38,
+  })
+); // lux
