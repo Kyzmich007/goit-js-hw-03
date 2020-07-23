4331d9f26152e9e6b6d5ecfd9fd8e3102f63850b
Vladyslav Kuz'menko <Kyzmich007@ukr.net>
2020-07-23 21:17:58 +0300

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,27 @@
 "use strict";
+const countTotalSalary = function (employees) {
+  const arrayOfNumbers = Object.values(employees);
+  let sum = 0;
+  for (const nuber of arrayOfNumbers) {
+    sum += nuber;
+  }
+  return sum;
+};
+
+console.log(countTotalSalary({})); // 0
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  })
+); // 330
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  })
+); // 400
